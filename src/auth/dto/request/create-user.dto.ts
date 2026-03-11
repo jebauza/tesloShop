97192e5c79b7e2f6975892eaa09b2855a215aa77
Jorge <jebauza1989@gmail.com>
2026-03-11 18:07:44 -0500

    test(auth): add unit tests for dtos and interfaces

--- src/auth/dto/request/create-user.dto.ts
+++ src/auth/dto/request/create-user.dto.ts
@@ -1,32 +1,35 @@
-import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'juan@email.com', description: 'User email' })
+  @IsEmail()
+  @MaxLength(100)
+  email: string;
 
-@ApiProperty({ example: 'juan@email.com', description: 'User email' })
-@IsEmail()
-email: string;
+  @ApiProperty({ example: 'Juan Perez', description: 'User full name' })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(255)
+  fullname: string;
 
-@ApiProperty({ example: 'Juan Perez', description: 'User full name' })
-@IsString()
-@MinLength(1)
-fullname: string;
-
-@ApiProperty({
+  @ApiProperty({
     example: 'Abcd1234!',
     description: 'Password with uppercase, lowercase, and a number',
     minLength: 6,
     maxLength: 50,
-})
-@IsString()
-@MinLength(6)
-@MaxLength(50)
-@Matches(
-    /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
-    {
-        message: 'Password must contain uppercase, lowercase, and a number'
-    }
-)
-password: string;
+  })
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password must contain uppercase, lowercase, and a number',
+  })
+  password: string;
 }
-
--- src/auth/dto/request/login-user.dto.ts
+++ src/auth/dto/request/login-user.dto.ts
@@ -1,27 +1,29 @@
-import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+  @ApiProperty({ example: 'juan@email.com', description: 'User email' })
+  @IsEmail()
+  @MaxLength(100)
+  email: string;
 
-@ApiProperty({ example: 'juan@email.com', description: 'User email' })
-@IsEmail()
-email: string;
-
-@ApiProperty({
+  @ApiProperty({
     example: 'Abcd1234!',
     description: 'Password with uppercase, lowercase and a number',
     minLength: 6,
     maxLength: 50,
-})
-@IsString()
-@MinLength(6)
-@MaxLength(50)
-@Matches(
-    /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
-    {
-        message: 'Password must have an uppercase, a lowercase and a number'
-    }
-)
-password: string;
+  })
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password must have an uppercase, a lowercase and a number',
+  })
+  password: string;
 }
-
