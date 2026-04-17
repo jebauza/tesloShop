38672b0d502355dd453b014ee724732843602167
Jorge <jebauza1989@gmail.com>
2026-04-17 14:43:00 -0500

    test(auth): add unit tests for AuthService

--- src/common/helpers/errors.helper.ts
+++ src/common/helpers/errors.helper.ts
@@ -1,10 +1,18 @@
-import { BadRequestException, InternalServerErrorException, Logger, } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 
-export const handleDBException = (loggerName: string = 'Logger', error: any): never => {
+export const handleDBException = (
+  loggerName: string = 'Logger',
+  error: any,
+): never => {
   if (error.code === '23505') {
     throw new BadRequestException(error.detail);
   }
 
+  // console.log(error);
   new Logger(loggerName).error(error);
   throw new InternalServerErrorException('Internal server error');
 };
